bdc4622c8c44b1e0974c7474219d90548c3a6750
fran0770 <franmowat33@gmail.com>
2025-01-02 17:15:31 +0000

    altering chart on input change

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-const yValues = [1, 1, 1, 1, 1];
-const xValues = ["option 1", "option 2", "option 3", "option 4", "option 5"];
+let yValues = [1, 1, 1, 1, 1];
+let xValues = ["option 1", "option 2", "option 3", "option 4", "option 5"];
 const barColors = ["#d50f25", "#eeb211", "#009925", "#3369e8", "#eeb211"];
+let chart;
 
 const spinWheel = () => {
     let value = Math.ceil(Math.random() * 3600);
     rotation += value;
+    const wheel = document.getElementById("wheel");
     wheel.style.transform = "rotate(" + rotation + "deg)";
 }
 
-new Chart("wheel", {
-    type: "pie",
-    data: {
-        datasets: [{
-        backgroundColor: barColors,
-        data: yValues, 
-        hoverBackgroundColor: barColors
-        }],
-        labels: xValues
-    }, 
-    options: {
-        plugins: {
-            datalabels: {
-                color: "#ffffff",
-                formatter: function(value, context) { 
-                    return context.chart.data.labels[context.dataIndex]; 
-                }, 
-                font: {
-                    size: 20
-                }
-            }
-        },
-        legend: {
-        display: false
-        },
-        tooltips: {
-        enabled: false
-        }, 
-        animation: {
-        duration: 0  
-        },
-        maintainAspectRatio: false,
-    }, 
-    plugins: [ChartDataLabels]
-});
+const handleInput = (e) => {
+    let inputValues = e.target.value.split("\n");
+    inputValues = inputValues.filter(item => item !== "");
 
-const wheel = document.getElementById("wheel");
+    xValues = inputValues;
+    yValues = Array(inputValues.length).fill(1);
+    generateChart();
+}
+
+const generateChart = () => {
+    const wheel = document.getElementById("wheel");
+
+    if (chart){
+        chart.destroy();
+    }
+
+    chart = new Chart("wheel", {
+        type: "pie",
+        data: {
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues, 
+            hoverBackgroundColor: barColors
+            }],
+            labels: xValues
+        }, 
+        options: {
+            plugins: {
+                datalabels: {
+                    color: "#ffffff",
+                    formatter: function(value, context) { 
+                        return context.chart.data.labels[context.dataIndex]; 
+                    }, 
+                    font: {
+                        size: 20
+                    }
+                }
+            },
+            legend: {
+            display: false
+            },
+            tooltips: {
+            enabled: false
+            }, 
+            animation: {
+            duration: 0  
+            },
+            maintainAspectRatio: false,
+        }, 
+        plugins: [ChartDataLabels]
+    });
+}
+
 const button = document.getElementById("spin-button");
 let rotation = 0; 
 button.addEventListener("click", spinWheel);
 
+const input = document.getElementsByTagName("textarea")[0];
+input.addEventListener("input", (e) => handleInput(e))
